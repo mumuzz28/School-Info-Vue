59309644c7aee60a216a6a0011f05f349f1a8666
28599 <2859953803@qq.com>
2024-10-02 17:52:22 +0800

    初始化

---
+++ my-phone-demo/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const value = ref('');
+</script>
+
+<template>
+  <img src="@/assets/aaa.jpg">
+  <HelloWorld />
+  <div>
+    <!-- 可以使用 CellGroup 作为容器 -->
+    <van-cell-group inset>
+      <van-field v-model="value" label="文本" placeholder="请输入用户名" />
+    </van-cell-group>
+  </div>
+
+</template>
+
+<style scoped></style>
---
+++ my-phone-demo/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+function handleclick() {
+  console.log("我被点击了")
+}
+
+</script>
+
+<template>
+  <van-button plain type="primary">朴素按钮</van-button>
+  <van-button plain type="success">朴素按钮</van-button>
+  <van-button type="primary" size="large">大号按钮</van-button>
+
+  <van-button size="large" type="primary" @click="handleclick">主要按钮</van-button>
+  <van-button size="large" type="success">成功按钮</van-button>
+  <van-button size="large" type="default">默认按钮</van-button>
+  <van-button size="large" type="danger">危险按钮</van-button>
+  <van-button size="large" type="warning">警告按钮</van-button>
+
+</template>
+
+<style scoped></style>
